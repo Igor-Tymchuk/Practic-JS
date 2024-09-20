3bc79ba065948f658d34dc0e67bb462ed0d7674b
Igor Tymchuk <167434833+Igor-Tymchuk@users.noreply.github.com>
2024-09-20 18:19:27 +0300

    ```

--- main.js
+++ main.js
@@ -3,14 +3,7 @@ const menuList = document.querySelector(".modal-overlay")
 const openMenu = () => {
     menuList.classList.toggle("is-open");
     regLogWindow.classList.remove("is-open");
-
 }
-menuBtn.addEventListener("click", openMenu);
-
-const time = document.querySelector(".time");
-time.textContent = `Time: ${new Date().getHours().toString().padStart(2, '0')}:
-${new Date().getMinutes().toString().padStart(2, '0')}`;
-
 
 const regLogBtn = document.querySelector(".reg-log");
 const regLogWindow = document.querySelector(".reg-log-overlay");
@@ -21,11 +14,26 @@ const openRegWindow = () => {
 regLogBtn.addEventListener("click", openRegWindow);
 
 const swapBtn = document.querySelector(".swap-form");
-
 const swapForm = () => {
     document.querySelector(".register").classList.toggle("none");
     document.querySelector(".login").classList.toggle("none");
-    if(swapBtn.textContent = "or Register!") swapBtn.textContent = "or Login!";
-    else swapBtn.textContent = "or Register!";
+    if (swapBtn.textContent != "or Login") swapBtn.textContent = "or Login";
+    else swapBtn.textContent = "or Register";
 }
 swapBtn.addEventListener("click", swapForm);
+menuBtn.addEventListener("click", openMenu);
+
+const currentDate = () => {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const day = now.getDate().toString().padStart(2, '0');
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const year = now.getFullYear();
+    document.querySelector(".time").textContent = `Time: ${hours}:${minutes}`;
+    return `${hours}:${minutes} (${day}:${month}:${year})`;
+}
+currentDate();
+
+
+
